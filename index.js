28b7dfdce8623d17e5e685779cd896950a2a4d86
BloodyPudding <bloodypudding@gmail.com>
2020-05-25 00:32:06 +0300

    Игра игрулина игруха

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const log = require('./module/log')
+const Logger = require('./module/logger')
+const logger = new Logger('logs.json')
 
-log.createFile()
+const Game = require('./module/game')
+const game = new Game()
+
+game.on('start', () => {
+    logger.createFile()
+})
+
+game.on('round', (log) => {
+    logger.add(log)
+})
+
+game.on('end', () => {
+    logger.read()
+})
+
+if (process.argv[2] === 'logs') {
+    logger.read()
+} else {
+    game.startGame()
+}
