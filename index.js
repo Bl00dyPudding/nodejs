0cb22d2c6dfe8cb79ca81d4890402b5b1c13a741
BloodyPudding <bloodypudding@gmail.com>
2020-05-28 15:52:55 +0300

    news

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-// const http = require('http');
-//
-// http.createServer((request, response) => {
-//     console.log('123');
-//     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-//     response.write('Привет, милая :3');
-//     response.end();
-// }).listen(80, '0.0.0.0');
-
-const http    = require('http')
-const url     = require('url')
-const path    = require('path')
-const fs      = require('fs')
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
 const request = require('request')
+const cheerio = require('cheerio')
 
 const port = 80
 const host = '0.0.0.0'
@@ -46,28 +38,44 @@ const requestHandler = (req, res) => {
     }
 
     if (req.method === 'POST') {
-        const body = [];
+        if (pathname === '/api/translate') {
+            const body = [];
 
-        req.on('data', data => {
-            body.push(Buffer.from(data))
-        })
-
-        req.on('end', () => {
-            const message = body.toString()
-
-            request.get(
-                `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200526T175332Z.60246197a1c49b55.7ca17cb0f5cb3c8fc24748158b632f70eb6e9a0a&lang=en-ru&text=${message}`,
-                (err, response, body) => {
-                if (err) throw err
-                console.log('statusCode:', response && response.statusCode)
-                res.end(body)
+            req.on('data', data => {
+                body.push(Buffer.from(data))
             })
-        })
+
+            req.on('end', () => {
+                const message = body.toString()
+
+                request.get(
+                    `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200526T175332Z.60246197a1c49b55.7ca17cb0f5cb3c8fc24748158b632f70eb6e9a0a&lang=en-ru&text=${message}`,
+                    (err, response, body) => {
+                        if (err) throw err
+                        console.log('statusCode:', response && response.statusCode)
+                        const answer = JSON.parse(body).text[0].replace(/"/g, '')
+                        res.end(JSON.stringify(answer))
+                    })
+            })
+        } else if (pathname === '/api/news') {
+            request.get(
+                `https://yandex.ru`,
+                (err, response, body) => {
+                    if (err) throw err
+                    console.log('statusCode:', response && response.statusCode)
+                    const $ = cheerio.load(body)
+                    const news = $('.list__item-content').map((i, el) => {
+                        return $(el).text()
+                    }).get()
+                    console.log(news)
+                    res.end(JSON.stringify(news))
+                })
+        }
     }
 
 }
 
-http.createServer(requestHandler).listen(port, host, (err) => {
+http.createServer(requestHandler).listen(port, host, err => {
     if (err) throw err
     console.log(`server is listening on ${host}:${port}`)
 })
