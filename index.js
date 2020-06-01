c2f5be75269929600005df9b834bc8c7bc092cda
BloodyPudding <bloodypudding@gmail.com>
2020-06-01 13:17:52 +0300

    hbs

--- index.js
+++ index.js
@@ -1,81 +1,37 @@
-const http = require('http')
-const url = require('url')
-const path = require('path')
-const fs = require('fs')
-const request = require('request')
-const cheerio = require('cheerio')
+const express = require('express')
+const exphbs  = require('express-handlebars')
+const path    = require('path')
 
-const port = 80
-const host = '0.0.0.0'
+const app  = express()
+const port = process.env.PORT || 80
+const hbs  = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
-const getContent = (name, res) => {
-    fs.readFile(
-        path.join(__dirname, 'views', `${name}.html`),
-        'utf-8',
-        (err, content) => {
-            if (err) throw err
-            res.end(content)
-        }
-    )
-}
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', path.resolve(__dirname, 'views'))
 
-const requestHandler = (req, res) => {
+app.use('/script', express.static(path.resolve(__dirname, 'public', 'script')))
+app.use('/style', express.static(path.resolve(__dirname, 'public', 'css')))
 
-    res.setHeader("Content-Type", "text/html; charset=utf-8;")
+app.listen(port, '0.0.0.0', () => {
+    console.log(`I'm back! Server is running on port ${port}`)
+})
 
-    const pathname = url.parse(req.url, true).pathname
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        message: `I'm back! Server is running on port ${port}`,
+        title: 'Главная',
+        isMain: true
+    })
+})
 
-    if (req.method === 'GET') {
-        if (pathname === '/') {
-            getContent('index', res)
-        } else if (pathname === '/translate') {
-            getContent('translate', res)
-        } else if (pathname === '/news') {
-            getContent('news', res)
-        } else {
-            getContent('index', res)
-        }
-    }
-
-    if (req.method === 'POST') {
-        if (pathname === '/api/translate') {
-            const body = [];
-
-            req.on('data', data => {
-                body.push(Buffer.from(data))
-            })
-
-            req.on('end', () => {
-                const message = body.toString()
-
-                request.get(
-                    `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200526T175332Z.60246197a1c49b55.7ca17cb0f5cb3c8fc24748158b632f70eb6e9a0a&lang=en-ru&text=${message}`,
-                    (err, response, body) => {
-                        if (err) throw err
-                        console.log('statusCode:', response && response.statusCode)
-                        const answer = JSON.parse(body).text[0].replace(/"/g, '')
-                        res.end(JSON.stringify(answer))
-                    })
-            })
-        } else if (pathname === '/api/news') {
-            request.get(
-                `https://yandex.ru`,
-                (err, response, body) => {
-                    if (err) throw err
-                    console.log('statusCode:', response && response.statusCode)
-                    const $ = cheerio.load(body)
-                    const news = $('.list__item-content').map((i, el) => {
-                        return $(el).text()
-                    }).get()
-                    console.log(news)
-                    res.end(JSON.stringify(news))
-                })
-        }
-    }
-
-}
-
-http.createServer(requestHandler).listen(port, host, err => {
-    if (err) throw err
-    console.log(`server is listening on ${host}:${port}`)
+app.get('/news', (req, res, next) => {
+    res.render('news', {
+        message: `Привет! Это новости!`,
+        title: 'Новости',
+        isNews: true
+    })
 })
