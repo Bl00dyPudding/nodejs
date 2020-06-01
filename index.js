6a9d0a8f1c71b18e02de798647445ace5c1c3fe4
BloodyPudding <bloodypudding@gmail.com>
2020-06-01 16:30:27 +0300

    новости

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs  = require('express-handlebars')
 const path    = require('path')
+const cheerio = require('cheerio')
+const request = require('request')
 
 const app  = express()
 const port = process.env.PORT || 80
@@ -12,7 +14,8 @@ const hbs  = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', path.resolve(__dirname, 'views'))
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use('/script', express.static(path.resolve(__dirname, 'public', 'script')))
 app.use('/style', express.static(path.resolve(__dirname, 'public', 'css')))
 
@@ -22,16 +25,42 @@ app.listen(port, '0.0.0.0', () => {
 
 app.get('/', (req, res, next) => {
     res.render('index', {
-        message: `I'm back! Server is running on port ${port}`,
         title: 'Главная',
         isMain: true
     })
 })
 
-app.get('/news', (req, res, next) => {
-    res.render('news', {
-        message: `Привет! Это новости!`,
-        title: 'Новости',
-        isNews: true
-    })
+app.get('/news/', (req, res, next) => {
+    request.get(
+        `https://yandex.ru`,
+        (err, response, body) => {
+            if (err) throw err
+            const $ = cheerio.load(body)
+            let news = $('.list__item-content').map((i, el) => {
+                return $(el).text()
+            }).get()
+
+            news = news.slice(0, req.query.limit)
+
+            res.render('news', {
+                news: news,
+                title: 'Новости',
+                isNews: true
+            })
+        })
+
+})
+
+app.post('/api/news', (req, res) => {
+    request.get(
+        `https://yandex.ru`,
+        (err, response, body) => {
+            if (err) throw err
+            const $ = cheerio.load(body)
+            let news = $('.list__item-content').map((i, el) => {
+                return $(el).text()
+            }).get()
+
+            res.end(JSON.stringify(news.slice(0, req.body.data)))
+        })
 })
--- public/script/main.js
+++ public/script/main.js
@@ -1,3 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     M.Sidenav.init(document.querySelectorAll('.sidenav'));
 });
+
+const createCard = (content) => {
+    const col = document.createElement('div');
+    col.classList.add('col', 's12', 'm6');
+    const card = document.createElement('div');
+    card.classList.add('card-panel', 'pink', 'lighten-2');
+    const cardContent = document.createElement('span');
+    cardContent.classList.add('white-text');
+    cardContent.innerText = content;
+    card.append(cardContent);
+    col.append(card);
+    return col;
+};
+
+const getNews = () => {
+    const value = document.querySelector('#range').value;
+    fetch('/api/news', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({data: value})
+    })
+        .then(res => res.json())
+        .then(data => {
+            const row = document.querySelector('.row');
+            row.innerHTML = '';
+            data.forEach(n => {
+                row.append(createCard(n));
+            });
+            history.pushState(null, null, `/news?limit=${value}`);
+        })
+        .catch(err => console.error(err));
+};
